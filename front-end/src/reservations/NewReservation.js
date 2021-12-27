4a9ce745ff5452da2a08a11c17e68e321cb83b10
Brandon Gamble-Thompson <brandondgt81@gmail.com>
2021-12-26 18:25:29 -0700

    made sure every apicalls had a catch

--- front-end/src/reservations/NewReservation.js
+++ front-end/src/reservations/NewReservation.js
@@ -1,5 +1,3 @@
-
-
 import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import ErrorAlert from "../layout/ErrorAlert";
@@ -39,7 +37,8 @@ export default function NewReservation({ loadDashboard, edit }) {
               reservation.reservation_id === Number(reservation_id)
           )
         )
-        .then(fillFields);
+        .then(fillFields)
+        .catch((error) => console.log(error));
     }
 
     function fillFields(foundReservation) {
@@ -64,12 +63,16 @@ export default function NewReservation({ loadDashboard, edit }) {
     }
 
     /** lists reservations for the given date once the new reservation has been created */
+
     async function loadReservations() {
-      const abortController = new AbortController();
-      /** uses listReservations() to send a get request to display all reservations for the given date */
-      return await listReservations(null, abortController.signal).catch(
-        setReservationsError
-      );
+      try {
+        const abortController = new AbortController();
+        /** uses listReservations() to send a get request to display all reservations for the given date */
+        return await listReservations(null, abortController.signal);
+      } catch (error) {
+        console.log(error);
+        setReservationsError(error)
+      }
     }
   }, [edit, reservation_id]);
 
@@ -178,111 +181,112 @@ export default function NewReservation({ loadDashboard, edit }) {
 
   /** displays the reservation form to the user */
   return (
-   <div style={{fontFamily: "Rubik"}}>
-     <h2 className="font-weight-bold d-flex justify-content-center mt-4">New Reservation</h2>
+    <div style={{ fontFamily: "Rubik" }}>
+      <h2 className="font-weight-bold d-flex justify-content-center mt-4">
+        New Reservation
+      </h2>
       <div className="d-flex justify-content-center">
-      <form className="font-weight-bold mt-3 m-3 w-75">
-        {errorsJSX()}
-        <ErrorAlert error={apiError} />
-        <ErrorAlert error={reservationsError} />
-        <label className="form-label" htmlFor="first_name">
-          First Name&nbsp;
-        </label>
-        <input
-          name="first_name"
-          id="first_name"
-          className="form-control mb-3 border-dark"
-          type="text"
-          onChange={handleChange}
-          value={formData.first_name}
-          required
-        />
-        <label className="form-label" htmlFor="last_name">
-          Last Name&nbsp;
-        </label>
-        <input
-          name="last_name"
-          id="last_name"
-          className="form-control mb-3 border-dark"
-          type="text"
-          onChange={handleChange}
-          value={formData.last_name}
-          required
-        />
-        <label className="form-label" htmlFor="mobile_number">
-          Mobile Number&nbsp;
-        </label>
-        <input
-          className="form-control mb-3 border-dark"
-          name="mobile_number"
-          id="mobile_number"
-          type="text"
-          onChange={handleChange}
-          value={formData.mobile_number}
-          required
-        />
+        <form className="font-weight-bold mt-3 m-3 w-75">
+          {errorsJSX()}
+          <ErrorAlert error={apiError} />
+          <ErrorAlert error={reservationsError} />
+          <label className="form-label" htmlFor="first_name">
+            First Name&nbsp;
+          </label>
+          <input
+            name="first_name"
+            id="first_name"
+            className="form-control mb-3 border-dark"
+            type="text"
+            onChange={handleChange}
+            value={formData.first_name}
+            required
+          />
+          <label className="form-label" htmlFor="last_name">
+            Last Name&nbsp;
+          </label>
+          <input
+            name="last_name"
+            id="last_name"
+            className="form-control mb-3 border-dark"
+            type="text"
+            onChange={handleChange}
+            value={formData.last_name}
+            required
+          />
+          <label className="form-label" htmlFor="mobile_number">
+            Mobile Number&nbsp;
+          </label>
+          <input
+            className="form-control mb-3 border-dark"
+            name="mobile_number"
+            id="mobile_number"
+            type="text"
+            onChange={handleChange}
+            value={formData.mobile_number}
+            required
+          />
 
-        <label className="form-label" htmlFor="reservation_date">
-          Reservation Date&nbsp;
-        </label>
-        <input
-          name="reservation_date"
-          id="reservation_date"
-          className="form-control mb-3 border-dark"
-          type="date"
-          placeholder="YYYY-MM-DD"
-          pattern="\d{4}-\d{2}-\d{2}"
-          onChange={handleChange}
-          value={formData.reservation_date}
-          required
-        />
-        <label className="form-label" htmlFor="reservation_time">
-          Reservation Time&nbsp;
-        </label>
-        <input
-          name="reservation_time"
-          id="reservation_time"
-          className="form-control mb-3 border-dark"
-          type="time"
-          placeholder="HH:MM"
-          pattern="[0-9]{2}:[0-9]{2}"
-          onChange={handleChange}
-          value={formData.reservation_time}
-          required
-        />
-        <label className="form-label" htmlFor="people">
-          Party Size&nbsp;
-        </label>
-        <input
-          name="people"
-          id="people"
-          className="form-control mb-3 border-dark"
-          type="number"
-          min="1"
-          onChange={handleChange}
-          value={formData.people}
-          required
-        />
-        <div className="d-flex justify-content-center">
-          <button
-            className="btn btn-outline-0 btn-success border-dark m-1" 
-            // style={{color: "white"}}
-            type="submit"
-            onClick={handleSubmit}
-          >
-            Submit
-          </button>
-          <button
-            className="btn btn-danger m-1"
-            type="button"
-            onClick={history.goBack}
-          >
-            Cancel
-          </button>
-        </div>
-      </form>
+          <label className="form-label" htmlFor="reservation_date">
+            Reservation Date&nbsp;
+          </label>
+          <input
+            name="reservation_date"
+            id="reservation_date"
+            className="form-control mb-3 border-dark"
+            type="date"
+            placeholder="YYYY-MM-DD"
+            pattern="\d{4}-\d{2}-\d{2}"
+            onChange={handleChange}
+            value={formData.reservation_date}
+            required
+          />
+          <label className="form-label" htmlFor="reservation_time">
+            Reservation Time&nbsp;
+          </label>
+          <input
+            name="reservation_time"
+            id="reservation_time"
+            className="form-control mb-3 border-dark"
+            type="time"
+            placeholder="HH:MM"
+            pattern="[0-9]{2}:[0-9]{2}"
+            onChange={handleChange}
+            value={formData.reservation_time}
+            required
+          />
+          <label className="form-label" htmlFor="people">
+            Party Size&nbsp;
+          </label>
+          <input
+            name="people"
+            id="people"
+            className="form-control mb-3 border-dark"
+            type="number"
+            min="1"
+            onChange={handleChange}
+            value={formData.people}
+            required
+          />
+          <div className="d-flex justify-content-center">
+            <button
+              className="btn btn-outline-0 btn-success border-dark m-1"
+              // style={{color: "white"}}
+              type="submit"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+            <button
+              className="btn btn-danger m-1"
+              type="button"
+              onClick={history.goBack}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-   </div>
   );
 }
-
